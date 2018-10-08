aaaf6da28d57dfbd4a0cc5ae993207b502f65e3b
Ketan Ramteke <ketantoo@gmail.com>
2018-10-08 22:19:30 +0530

    "3 sections completed"

---
+++ Python Interactive Problems Solutions/solution.py
@@ -0,0 +1,133 @@
+# Write the statement below!
+print("Python")
+
+# Write the statement below!
+print("Hello "+"World")
+
+# Fix the statements below
+print("Python was named after the comedy troupe Monty Python")
+print("Python was created in 1991 by Guido Van Rossumir!")
+
+# Define the variables below
+language = "Python"
+year = 2018
+
+#python_version = 3
+#In this course, you will be using the newest python version
+
+# Write the code below
+x = 4+3
+y = 10-1
+z = 9*8
+w = 6/2
+
+print(x)
+print(y)
+print(z)
+print(w)
+modulo = 999%7
+print(modulo)
+
+
+# Write your code below
+price = 39.99
+print(price)
+new_price = 19.99
+price -= new_price
+print(price)
+
+# Write the code below
+apples = 8
+price_per_apple = 0.5
+cost = apples*price_per_apple
+print(cost)
+
+# Write you code below
+python = """Python is one 
+of the official languages 
+at Google!"""
+print(python)
+
+#Python is an open source language.
+#It can be used freely for any kind of purpose.
+python_is_free = True
+python_is_closed_source = False
+
+# Create the variable below
+jeff = "My name is Jeff!"
+
+# Write the code below
+third_letter = "BitDegree"[2]
+print(third_letter)
+
+# Write the code below
+car = "Cadillac"
+print(len(car))
+
+# Call the method below
+car = "Cadillac"
+print(car.lower())
+
+
+# Call the method below
+car = "Cadillac"
+print(car.upper())
+
+#Declare the variable below
+e = 2.71828
+e = str(e)
+e = type(e)
+print(e)
+
+# Write the code below
+python = "Programming language"
+print(len(python))
+print(python.upper())
+
+# Write the print statement below
+print("Learn "+"and "+"earn")
+s1 = "Pythons"
+s2 = "snakes"
+print("%s are one of the largest %s." % (s1, s2))
+
+name = input("What is your name?")
+age = input("How old are you? ")
+hobby = input("What is your favourite hobby? ")
+# Update the code below
+print("Hello %s, I know that you are %s year old, " /
+"and your favourite hobby is %s." % (name, age, hobby))
+
+# After running the code, the program will load the module.
+from datetime import datetime
+print("The program loaded the module...")
+
+
+# Define the variable below
+from datetime import datetime
+now = datetime.now()
+print(now)
+
+#Write the code below
+from datetime import datetime
+now = datetime.now()
+print(now.year)
+print(now.month)
+print(now.day)
+
+# Write the print statement below
+from datetime import datetime
+now = datetime.now()
+print("%02d/%04d/%02d"%(now.day,now.year,now.month))
+
+# Write the code below
+from datetime import datetime
+now = datetime.now()
+print(now.minute)
+print(now.second)
+print("%02d:%02d:%02d"%(now.second,now.minute,now.hour))
+
+# Write the code below
+from datetime import datetime
+now = datetime.now()
+print("%02d/%02d/%04d"%(now.day,now.month,now.year)+" %02d:%02d:%02d"%(now.second,now.minute,now.hour))
+
